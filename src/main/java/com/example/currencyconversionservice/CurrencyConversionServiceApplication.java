9933b5f690def7bfc2f252ebfa3c9b5fd09abc4b
David <yangda@msoe.edu>
2022-09-27 23:02:04 -0500

    Initial commit

---
+++ src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+// http://localhost:8100/currency-conversion-feign/from/USD/to/INR/quantity/10
+// Conversion
+@SpringBootApplication
+@EnableFeignClients
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
